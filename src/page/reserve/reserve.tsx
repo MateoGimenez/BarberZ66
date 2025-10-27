1522ab8e20c92c6b288a31da5ccd8a177220085a
Mateo Gimenez <matex23gimenez11@gmail.com>
2025-10-27 17:19:53 -0300

    i add the calendar

--- src/page/reserve/reserve.tsx
+++ src/page/reserve/reserve.tsx
@@ -1,8 +1,40 @@
-import './reserve.css'
+import { useState } from "react";
+import { DayPicker } from "react-day-picker";
+import { es } from "react-day-picker/locale";
+import "react-day-picker/style.css";
+import "./reserve.css";
+
 export default function Reserve() {
+  const [selected, setSelected] = useState<Date>();
+
   return (
     <section>
-      <h1>Reserve Page</h1>
+      <h1>Reserva tu turno</h1>
+      <div className="contenedor-calendario">
+        <DayPicker
+          mode="single"
+          locale={es}
+          disabled={(day) => day < new Date()}
+          selected={selected}
+          onSelect={setSelected}
+          footer={
+            selected
+              ? `Has elegido el ${selected.toLocaleDateString("es-ES", {
+                  weekday: "long",
+                  day: "numeric",
+                  month: "long",
+                })}`
+              : "Elige un día para tu turno"
+          }
+          className="my-calendar"
+        />
+      </div>
+
+      <div className="contenedor-horario">
+        aca van los horarios
+      </div>
+
+      <button>Reservar</button>
     </section>
-  )
+  );
 }
